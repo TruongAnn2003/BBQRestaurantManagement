232d36fbaddc0f935796e09c54169f5809eb2cba
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-04-20 11:14:09 +0700

    #20 add view menu and logo homeview

---
+++ BBQRestaurantManagement/Services/AlertDialogService.cs
@@ -0,0 +1,49 @@
+﻿using BBQRestaurantManagement.ViewModels.Base;
+using System;
+using BBQRestaurantManagement.ViewModels.Dialogs;
+using BBQRestaurantManagement.Views.Dialogs;
+
+namespace BBQRestaurantManagement.Services
+{
+    public class AlertDialogService
+    {
+        private AlertDialog alertDialog = new AlertDialog();
+        private AlertDialogViewModel viewmodel;
+
+        private Action yesAction;
+        private Action noAction;
+
+        public AlertDialogService(string title, string message, Action yesAction, Action noAction)
+        {
+            viewmodel = alertDialog.ViewModel;
+            viewmodel.Title = title;
+            viewmodel.Message = message;
+            this.yesAction = yesAction;
+            this.noAction = noAction;
+            SetCommands();
+        }
+
+        private void SetCommands()
+        {
+            viewmodel.YesCommand = new RelayCommand<object>(ExecuteYesCommand);
+            viewmodel.NoCommand = new RelayCommand<object>(ExecuteNoCommand);
+        }
+
+        private void ExecuteYesCommand(object obj)
+        {
+            yesAction?.Invoke();
+            alertDialog.Close();
+        }
+
+        private void ExecuteNoCommand(object obj)
+        {
+            noAction?.Invoke();
+            alertDialog.Close();
+        }
+
+        public void Show()
+        {
+            alertDialog.ShowDialog();
+        }
+    }
+}
---
+++ BBQRestaurantManagement/ViewModels/Dialogs/AlertDialogViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BBQRestaurantManagement.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BBQRestaurantManagement.ViewModels.Dialogs
+{
+    public class AlertDialogViewModel : BaseViewModel
+    {
+        private string title = "";
+        public string Title { get => title; set { title = value; OnPropertyChanged(); } }
+
+        private string message = "";
+        public string Message { get => message; set { message = value; OnPropertyChanged(); } }
+
+        public ICommand YesCommand { get; set; }
+        public ICommand NoCommand { get; set; }
+    }
+}
