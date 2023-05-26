43706532282bb3d1eb547d3c3e26b24d88e543cb
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-05-27 02:47:09 +0700

    #55 fix error order and create userview

---
+++ BBQRestaurantManagement/Views/UserControls/UserInfomationUC.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BBQRestaurantManagement.ViewModels.UserControls;
+using System.Windows.Controls;
+
+namespace BBQRestaurantManagement.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UserInfomationUC.xaml
+    /// </summary>
+    public partial class UserInfomationUC : UserControl
+    {
+        public UserInfomationUC()
+        {
+            InitializeComponent();
+            DataContext = new UserInfomationViewModel();
+        }
+    }
+}
