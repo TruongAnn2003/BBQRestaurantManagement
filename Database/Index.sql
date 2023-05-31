8027519868985863a96df96a046f14e25fc019fc
your_username <nguyenthanhloi260303@gmail.com>
2023-05-31 13:46:57 +0700

    #69 index 3 table

---
+++ Database/Index.sql
@@ -0,0 +1,1 @@
+-- Lọc khách hàng theo số điện thoạiCREATE INDEX IndNumPhone ON Customers (NumberPhone)-- Lọc sản phẩm theo loại CREATE INDEX IndProdTyp ON Product (Product_Type)--Lọc OrderDetails theo OrderCREATE INDEX IndOrderID ON OrderDetails (OrderID)
